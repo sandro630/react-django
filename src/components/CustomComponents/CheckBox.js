6e8cb693f3f03ed96e7185e6d0c3198bd4d1176b
sandro <chapidzesandro39@gmail.com>
2019-11-26 10:59:08 +0200

    v1

--- src/components/CustomComponents/CheckBox.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const CheckBox = props => {
-    return (
-       <input key={props.id} onChange={props.handleCheckChieldElement} type="checkbox" checked={props.isChecked} value={props.value} />
-    )
-}
-
-export default CheckBox
--- src/containers/EditProfileContainer.js
+++
@@ -1,9 +0,0 @@
-import { connect } from "react-redux";
-
-import EditProfile from "../components/EditProfile";
-
-const mapStateToProps = state => ({
-    cordata: state.cordata,
-});
-
-export default connect(mapStateToProps)(EditProfile);
